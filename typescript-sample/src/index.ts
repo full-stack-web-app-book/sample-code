fe6de4a84521134a91e46c001e9f9136c3da8d02
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-07-27 16:23:24 +0900

    feat: TypeScript のサンプルを追加

---
+++ typescript-sample/src/index.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const num1: number = 10;
+const num2 = 3;
+const sum = add(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is:`, sum);

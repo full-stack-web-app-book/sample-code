3421dc36c5380a359f4ca8f9105d1957109d89e2
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-08-10 11:29:49 +0900

    update: ファイル読み取りのモックに実装を変更

--- nodejs-sample-app/non-blocking.js
+++ nodejs-sample-app/non-blocking.js
@@ -1,38 +1,37 @@
-// データベースクエリをシミュレートする関数（実行時間3秒）
-function databaseQuery() {
+// ファイル読み込みをシミュレートする関数（実行時間3秒）
+function readFileMock() {
   return new Promise((resolve) => {
-    console.log("データベースクエリを開始...");
+    // 処理の実行を開始
+    console.log("【ファイル読み込み】ファイル読み込みを開始...");
 
-    // 3秒後に解決されるPromiseを返す
+    // 3秒後に以下の処理を実行する
     setTimeout(() => {
-      const result = {
-        id: 1,
-        name: "サンプルデータ",
-        timestamp: new Date().toISOString(),
-      };
-      console.log("データベースクエリが完了しました");
+      const result = "Hello World!";
+      console.log("【ファイル読み込み】ファイル読み込みが完了しました");
       resolve(result);
-    }, 3000);
+    }, 3000); // 3000ms
   });
 }
 
 // メイン関数
 async function main() {
-  // 長時間(3秒間)の処理を開始
-  const longTask = databaseQuery();
+  // 長時間(3秒間)の処理を実行
+  const readFileResult = readFileMock();
 
   // その間に他の処理を実行
-  console.log("データベースクエリの実行中に他の処理を実行...");
-
+  console.log("===================");
+  console.log("ファイル読み込みの実行中に他の処理を実行...");
   for (let i = 1; i <= 5; i++) {
     console.log(`カウント: ${i}`);
     await new Promise((resolve) => setTimeout(resolve, 500)); // 0.5秒待機
   }
+  console.log("他の処理が完了しました");
+  console.log("===================");
 
   // 長時間の処理の完了を待つ
-  const result = await longTask;
-  console.log("データベースクエリの結果:", result);
-  console.log("");
+  const result = await readFileResult;
+  console.log("ファイル読み込みの結果");
+  console.log(result);
 }
 
 // 実行

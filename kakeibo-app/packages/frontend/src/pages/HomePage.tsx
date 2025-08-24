9c5545d752f1a60e26222904913539238a218ed2
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-08-24 11:36:10 +0900

    feat: 収支登録ボタンをアップデート

--- kakeibo-app/packages/frontend/src/pages/HomePage.tsx
+++ kakeibo-app/packages/frontend/src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Center, Container } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import BalanceSummary from "@/components/BalanceSummary";
 import TransactionHistory from "@/components/TransactionHistory";
 import { useTransactions } from "@/hooks/useTransactions";
 import { useFinancialSummary } from "@/hooks/useFinancialSummary";
+import { Center, Container, Flex, Stack } from "@chakra-ui/react";
+import InputButton from "@/components/InputButton";
 
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
   const {
     data: financialSummary,
     isLoading: isFinancialLoading,
@@ -34,23 +37,23 @@ const HomePage: React.FC = () => {
           <div className="error-message">{transactionsError.message}</div>
         )}
 
-        <BalanceSummary
-          totalIncome={financialSummary?.totalIncome || 0}
-          totalExpense={financialSummary?.totalExpense || 0}
-          balance={financialSummary?.balance || 0}
-          isLoading={isLoading}
-        />
+        <Stack gap={4}>
+          <Flex justifyContent="flex-end">
+            <InputButton onClick={() => navigate("/input")} />
+          </Flex>
 
-        <div className="button-container">
-          <Link to="/input" className="add-button">
-            収支を登録する
-          </Link>
-        </div>
+          <BalanceSummary
+            totalIncome={financialSummary?.totalIncome || 0}
+            totalExpense={financialSummary?.totalExpense || 0}
+            balance={financialSummary?.balance || 0}
+            isLoading={isLoading}
+          />
 
-        <TransactionHistory
-          incomeTransactions={incomeList?.transactions || []}
-          expenseTransactions={expenseList?.transactions || []}
-        />
+          <TransactionHistory
+            incomeTransactions={incomeList?.transactions || []}
+            expenseTransactions={expenseList?.transactions || []}
+          />
+        </Stack>
       </Container>
     </Center>
   );

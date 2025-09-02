8e0d25989804b7d7f1e56cb5f47a2f781fdd0204
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-09-02 22:51:25 +0900

    add: Chakra UI の導入

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button, Heading } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1>シンプル家計簿</h1>
+      <Heading as="h1">シンプル家計簿</Heading>
+      <Button colorPalette="teal" fontWeight="bold">
+        登録
+      </Button>
     </>
   );
 }

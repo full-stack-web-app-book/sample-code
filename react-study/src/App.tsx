c684630f2d075436a5429f017af519f77b3ef435
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-09-06 10:59:13 +0900

    feat: ヘッダーの作成

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,9 +1,20 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Heading as="h1">シンプル家計簿</Heading>
+      <Box
+        as="header"
+        bg="teal.500"
+        color="white"
+        py={4}
+        textAlign="center"
+        boxShadow="md"
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          シンプル家計簿
+        </Text>
+      </Box>
       <Button colorPalette="teal" fontWeight="bold">
         登録
       </Button>

09d3c6ac75fc6fc4e7a470b8a6ee5b655c60f439
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-09-06 11:12:29 +0900

    delete: App の Box を削除

--- kakeibo-app/packages/frontend/src/App.tsx
+++ kakeibo-app/packages/frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import InputPage from "./pages/InputPage";
@@ -7,13 +6,11 @@ import InputPage from "./pages/InputPage";
 function App() {
   return (
     <Router>
-      <Box minH="100vh">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/input" element={<InputPage />} />
-        </Routes>
-      </Box>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/input" element={<InputPage />} />
+      </Routes>
     </Router>
   );
 }

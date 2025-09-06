386f6939d46f01bdc967a0ed9f14efd6dc57a245
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2025-09-06 11:13:32 +0900

    delete: HomePage の Center を削除

--- kakeibo-app/packages/frontend/src/pages/HomePage.tsx
+++ kakeibo-app/packages/frontend/src/pages/HomePage.tsx
@@ -3,24 +3,22 @@ import { useNavigate } from "react-router-dom";
 import BalanceSummary from "@/components/BalanceSummary";
 import TransactionList from "@/components/TransactionList";
 import { useTransactions } from "@/hooks/useTransactions";
-import { Box, Center, Container, Flex, Stack } from "@chakra-ui/react";
+import { Box, Container, Flex, Stack } from "@chakra-ui/react";
 import InputButton from "@/components/InputButton";
 import SummaryCard from "@/components/SummaryCard";
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <Center>
-      <Container maxW="4xl" py={6}>
-        <Stack gap={4}>
-          <Flex justifyContent="flex-end">
-            <InputButton onClick={() => navigate("/input")} />
-          </Flex>
-          <BalanceSummary />
-          <TransactionHistory />
-        </Stack>
-      </Container>
-    </Center>
+    <Container maxW="4xl" py={6}>
+      <Stack gap={4}>
+        <Flex justifyContent="flex-end">
+          <InputButton onClick={() => navigate("/input")} />
+        </Flex>
+        <BalanceSummary />
+        <TransactionHistory />
+      </Stack>
+    </Container>
   );
 };
 
